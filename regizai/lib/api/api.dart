c82aefbaeb8c607308458a4236718e045e4a82e4
Akulohcuek0 <ibrahimkholilulafham@gmail.com>
2024-05-28 10:59:11 +0700

    refresh

--- regizai/lib/api/api.dart
+++ regizai/lib/api/api.dart
@@ -1,4 +1,4 @@
 class Api {
-  static const _host = "http://10.0.143.28/regizai_api";
-  static String login = "http://10.0.143.28/regizai_api/login.php";
+  static const _host = "http://10.0.141.210/regizai_api";
+  static String login = "http://10.0.141.210/regizai_api/login.php";
 }
